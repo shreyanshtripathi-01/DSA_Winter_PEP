8680a0cd27eb8796d290106f7c3cfd15d05e215b
Shreyansh Tripathi <Shreyanshcoder196@gmail.com>
2026-02-05 17:10:00 +0530

    valid palindrome ii done

---
+++ Homework/Strings/7.valid_palindrome_ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r])
+                return isPalin(s,l+1,r)||isPalin(s,l,r-1);
+            l++;r--;
+        }
+        return true;
+    }
+    bool isPalin(string& s, int l, int r) {
+        while(l<r){
+            if(s[l]!=s[r]) return false;
+            l++;r--;
+        }
+        return true;
+    }
+};
