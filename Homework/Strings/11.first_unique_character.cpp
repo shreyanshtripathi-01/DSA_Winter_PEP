607f777eb72a643d5eccdeb1210627031457768f
Shreyansh Tripathi <Shreyanshcoder196@gmail.com>
2026-02-06 14:30:00 +0530

    add strings and first unique char

---
+++ Homework/Strings/11.first_unique_character.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> cnt(26,0);
+        for(char c:s) cnt[c-'a']++;
+        for(int i=0;i<s.size();i++){
+            if(cnt[s[i]-'a']==1) return i;
+        }
+        return -1;
+    }
+};
