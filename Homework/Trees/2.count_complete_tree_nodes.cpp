70f5a2a29406aacf6f5a8c1facecacfb107de220
Shreyansh Tripathi <Shreyanshcoder196@gmail.com>
2026-02-18 14:30:00 +0530

    count complete tree nodes

---
+++ Homework/Trees/2.count_complete_tree_nodes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        int lh=0,rh=0;
+        TreeNode* l=root,*r=root;
+        while(l){lh++;l=l->left;}
+        while(r){rh++;r=r->right;}
+        if(lh==rh) return (1<<lh)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
